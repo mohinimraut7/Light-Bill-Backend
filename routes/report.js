2891952bc3f1196374f592fa466e0a36daf831b2
mohinimraut7 <mohinimraut7@gmail.com>
2025-04-03 00:30:02 +0530

    getReports api integrate

--- routes/report.js
+++ routes/report.js
@@ -1,7 +1,8 @@
 const express=require('express');
-const { addRemarkReports } = require('../controller/reports');
+const { addRemarkReports,getReports } = require('../controller/reports');
 const router=express.Router();
 
 router.post('/addRemarkReport',addRemarkReports)
+router.get('/getReports',getReports)
 
 module.exports=router;  
