03bcb0e13897d3ad4e4a81936759299b11ab5979
mohinimraut7 <mohinimraut7@gmail.com>
2026-04-24 17:46:21 +0530

    Energy Expenditure buttons ui changes

--- index.js
+++ index.js
@@ -19,20 +19,20 @@ const imageRoutes = require('./routes/imageRoute');
 app.use("/uploads", express.static("uploads"));
 const port = process.env.PORT || 5000;
 env.config();
-// mongoose
-//   .connect(
-//     `mongodb+srv://mohini:mohiniraut@cluster0.rxemnue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-//   )
-//   .then(() => {
-//     console.log("Database connected");
-//   });
 mongoose
   .connect(
-    `mongodb://127.0.0.1:27017/lightbill?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3`
+    `mongodb+srv://mohini:mohiniraut@cluster0.rxemnue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => {
     console.log("Database connected");
   });
+// mongoose
+//   .connect(
+//     `mongodb://127.0.0.1:27017/lightbill?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.3.3`
+//   )
+//   .then(() => {
+//     console.log("Database connected");
+//   });
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
   app.use('/api',addUserRoutes)
